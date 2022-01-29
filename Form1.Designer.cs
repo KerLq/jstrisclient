350653c0d3a1163d048bc1f81b5216fe47c3b6eb
KerLq <88601684+KerLq@users.noreply.github.com>
2022-01-29 22:27:53 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,52 @@
+﻿namespace Jstris_Client
+{
+    partial class Jstris
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Jstris));
+            this.SuspendLayout();
+            // 
+            // Jstris
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(835, 452);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "Jstris";
+            this.Text = "Jstris";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Jstris_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace Jstris_Client
+{
+    public partial class Jstris : Form
+    {
+        public ChromiumWebBrowser browser;
+        public Jstris()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.Sizable;
+            this.WindowState = FormWindowState.Maximized;
+            InitializeChromium();
+
+        }
+
+        public void InitializeChromium()
+        {
+            CefSettings settings = new CefSettings();
+            settings.CachePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\CEF";
+            Cef.Initialize(settings);
+            browser = new ChromiumWebBrowser("https://jstris.jezevec10.com");
+            browser.KeyboardHandler = new KeyboardHandler();
+            this.Controls.Add(browser);
+            browser.Dock = DockStyle.Fill;
+
+        }
+
+        private void Jstris_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                MessageBox.Show("SheesH");
+            }
+        }
+    }
+
+
+    //KeyboardHandler by tobre => https://stackoverflow.com/questions/60740328/keyboard-shortcut-does-not-make-it-to-cefsharp-browser-control
+    public class KeyboardHandler : IKeyboardHandler
+    {
+        private bool _cefAltKeyPressed;
+        public bool OnPreKeyEvent(IWebBrowser chromiumWebBrowser, IBrowser browser, KeyType type, int windowsKeyCode, int nativeKeyCode, CefEventFlags modifiers, bool isSystemKey, ref bool isKeyboardShortcut)
+        {
+            return false;
+        }
+
+        public bool OnKeyEvent(IWebBrowser chromiumWebBrowser, IBrowser browser, KeyType type, int windowsKeyCode, int nativeKeyCode, CefEventFlags modifiers, bool isSystemKey)
+        {
+            if (windowsKeyCode == 18)
+            {
+                _cefAltKeyPressed = true;
+            }
+            const int KEY_F4 = 115;
+            //Console.WriteLine("Key Pressed " + windowsKeyCode); .> To Debug and analyse other Shortcut Combinations
+            if (_cefAltKeyPressed)
+            {
+                if (windowsKeyCode == KEY_F4)
+                {
+
+                    Application.Exit();
+                }
+                
+                _cefAltKeyPressed = false;
+
+            }
+            return true;
+        }
+    }
+}
